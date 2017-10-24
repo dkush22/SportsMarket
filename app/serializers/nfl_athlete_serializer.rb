49aab76399f0f0b4d1f114ce64e8c3dcb2f3941c
dkush22 <dkush22@gmail.com>
2017-10-24 17:31:34 -0400

    didn't really do anything today on the backend but im going to commit

--- app/serializers/nfl_athlete_serializer.rb
+++ app/serializers/nfl_athlete_serializer.rb
@@ -1,5 +1,5 @@
 class NflAthleteSerializer < ActiveModel::Serializer
-  attributes :id, :name, :position, :team, :passing_yards, :passing_touchdowns, :rushing_yards, :rushing_touchdowns, :receiving_yards, :receiving_touchdowns, :initial_stock_value, :current_stock_value, :fantasy_projection, :actual_fantasy_value, :created_at
+  attributes :id, :name, :position, :team, :passing_yards, :passing_touchdowns, :rushing_yards, :rushing_touchdowns, :receiving_yards, :receiving_touchdowns, :initial_stock_value, :current_stock_value, :fantasy_projection, :actual_fantasy_value, :week_one, :week_two, :week_three, :week_four, :week_five, :week_six, :week_seven, :created_at
 
 # has_many :investments
 # has_many :users, through: :investments
